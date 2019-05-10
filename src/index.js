23456aa927585dfeebd57ec3b987a5dc88e2cf2c
Fardeem Munir <fardeem.munir@erubai.com>
2019-05-11 01:56:03 +0600

    Get updates from the main website

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
 import './styles/index.scss';
 
 
+import { el, mount } from 'redom';
+
+fetch('/api/posts')
+  .then(res => res.json())
+  .then(data => {
+    return data.map( ({ title, date, link }) => {
+      const dateInst = new Date(date);
+      const dateFormat = `${dateInst.toLocaleString('en-us', { month: 'long' })} ${dateInst.getDay()} ${dateInst.getYear() + 1900}`;
+      
+      return el('div.item', [
+        el('date', dateFormat),
+        el('h1', title.rendered),
+        el('a', { href: link }, [
+          el('button', 'Read More')
+        ]),
+      ]);
+    });
+  })
+  .then((updates) => {
+    mount(document.getElementById('updates'), el('div.updates', updates))
+  });
+
